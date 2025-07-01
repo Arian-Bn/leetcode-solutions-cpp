d0a7e14347dbcda793e16bc26080c3d1d452944a
Arian <arianbagheri2476@gmail.com>
2025-07-01 22:43:48 +0300

    Update: added comments

--- 005-length-of-last-word.cpp
+++ 005-length-of-last-word.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+/**
+ * LeetCode Problem #5: Length of Last Word
+ *
+ * Given a string s consisting of words and spaces, return the length
+ * of the last word in the string.
+ *
+ * A word is a maximal substring of non-space characters.
+ */
 
 int lengthOfLastWord(const string& s);
 
@@ -17,13 +27,24 @@ int main () {
     return 0;
 }
 
+/**
+ * Returns the length of the last word in the string.
+ *
+ * Steps:
+ * 1. Start from the end of the string
+ * 2. Skip trailing spaces
+ * 3. Count characters until the next space or start of string
+ */
+
 int lengthOfLastWord(const string& s) {
     int length = 0;
     int i = s.length() - 1;
 
+    // Skip trailing spaces at the end of the string
     while (i >= 0 && s[i] == ' ')
         i--;
 
+    // Count the length of the last word
     while (i >= 0 && s[i] != ' ') {
         length++;
         i--;
